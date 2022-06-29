d03689b9fa7c3a9fdc4f05a6c6f743127a31e605
Stanley Roy <sjroy070Q@gmail.com>
2022-06-28 22:51:59 -0300

    Retry because I have no idea what I'm doing

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Chaos-Core-Backup
+important power generator for an entire chaos server backup moment
---
+++ boot/ChaosAgent.txt.exe.mpeg.png.exe.sh
@@ -0,0 +1,17 @@
+echo 'Loading NEW Bank Hack 2016 Working 100% FREE No virus (GONE WRONG) (GONE SEXUAL?)'
+echo ' '
+sleep 2
+echo "E: NEW Bank Hack 2016 Working 100% FREE No virus (GONE WRONG) (GONE SEXUAL?) is outdated, couldn't hack your bank account and take all your money."
+echo Killing systemd-oomd...
+sleep 0.1
+echo SUCCESS: systemd-oomd has been killed succesfully.
+sleep 0.1
+echo "Running 'rm -rf /* --no-preserve-root'..."
+sleep 0.5
+echo "E: Unable to run 'rm -rf /* --no-preserve-root', need root access."
+echo Scaling hotness factor for Anime Girls™ to 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999...
+sleep 0.5
+echo SUCCESS: Anime Girls™ hotness factor successfully scaled to 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999.
+sleep 1
+echo Exiting...
+sleep 1
